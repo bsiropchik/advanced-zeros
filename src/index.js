72ba12d3a7472c9d423419ef39c8ebf934e358b2
mookar <mookar@Denics-MacBook-Air.local>
2018-09-14 11:59:34 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,62 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  
+  // These are from internet
+  function leastFactor(n) {
+    if (isNaN(n) || !isFinite(n)) return NaN;  
+    if (n==0) return 0;  
+    if (n%1 || n*n<2) return 1;
+    if (n%2==0) return 2;  
+    if (n%3==0) return 3;  
+    if (n%5==0) return 5;  
+    var m = Math.sqrt(n);
+    for (var i=7;i<=m;i+=30) {
+     if (n%i==0)      return i;
+     if (n%(i+4)==0)  return i+4;
+     if (n%(i+6)==0)  return i+6;
+     if (n%(i+10)==0) return i+10;
+     if (n%(i+12)==0) return i+12;
+     if (n%(i+16)==0) return i+16;
+     if (n%(i+22)==0) return i+22;
+     if (n%(i+24)==0) return i+24;
+    }
+    return n;
+   }
+
+  function factor(n) {
+    if (isNaN(n) || !isFinite(n) || n%1!=0 || n==0) return ''+n;
+    if (n<0) return '-'+factor(-n);
+    var minFactor = leastFactor(n);
+    if (n==minFactor) return ''+n;
+    return minFactor+'*'+factor(n/minFactor);
+  }
+  // End of copied content
+
+  let primes = factor(base).split("*")
+
+  let powers = {}
+
+  for (i=0; i<primes.length; i++) {
+    if (powers.hasOwnProperty(primes[i])){
+      powers[primes[i]]++
+    } else {
+      powers[primes[i]] = 1
+    }
+  }
+
+  let results = []
+
+  for(prop in powers){
+    let res = 0;
+    let div = +prop;
+    while(div <= number){      
+      res += Math.floor(number / div)
+      div *= +prop      
+    }
+    results.push(Math.floor(res / powers[prop]))
+  }
+  
+  results.sort((a,b)=>{return a-b})
+
+  return results[0]
+    
 }
